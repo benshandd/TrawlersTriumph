5bbe8e0f6ff63496388a792f846161091e1e2062
SlazengerV100 <kendreanth@ecs.vuw.ac.nz>
2023-09-14 16:20:03 +1200

    Add fields to InfoField, Key and Door

--- LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/domain/InfoField.java
+++ LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/domain/InfoField.java
@@ -1,7 +1,14 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
 public class InfoField extends Free {
-    public InfoField(Chap chap) {
+    private String message;
+
+    public InfoField(Chap chap, String message) {
         super(chap);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
--- LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/domain/Key.java
+++ LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/domain/Key.java
@@ -1,7 +1,18 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
 public class Key extends Free {
-    public Key(Chap chap) {
+    private Key.Colour colour;
+
+    public Key(Chap chap, Key.Colour colour) {
         super(chap);
+        this.colour = colour;
     }
+
+    ;
+
+    public Key.Colour getColour() {
+        return colour;
+    }
+
+    enum Colour {BLUE, GREEN, YELLOW, RED}
 }
