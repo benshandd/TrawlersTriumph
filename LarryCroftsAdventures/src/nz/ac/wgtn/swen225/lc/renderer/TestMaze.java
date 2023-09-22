2f8d83ccdab941f4b92a4396b5c84dfc08c8383e
Tokeley <137751232+Tokeley@users.noreply.github.com>
2023-09-22 16:35:34 +1200

    Made the colors of each tile change depending it's x y position

--- LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/renderer/TestMaze.java
+++ LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/renderer/TestMaze.java
@@ -9,8 +9,8 @@ public class TestMaze {
         Color c;
         for (int x = 0; x < 9; x++){
             for (int y = 0; y < 9; y++){
-                if ((x + y) % 2 == 0){ c = Color.CYAN; }
-                else {c = Color.LIGHT_GRAY; }
+                if ((x + y) % 2 == 0){ c = new Color(95+(x*10), 158+(x*10), 160+(x*10)); }
+                else {c = new Color(9+(y*10), 121+(y*10), 105+(y*10)); }
                 grid[x][y] = new TestTile(x, y, c);
             }
         }
