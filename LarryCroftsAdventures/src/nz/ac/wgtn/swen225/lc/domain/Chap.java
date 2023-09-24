ead41774fbde6c0256cdbe0f7ed80c626cdbc945
SlazengerV100 <kendreanth@ecs.vuw.ac.nz>
2023-09-25 11:40:37 +1300

    Add InventoryFull exception class

--- LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/domain/Chap.java
+++ LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/domain/Chap.java
@@ -1,12 +1,41 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
+import nz.ac.wgtn.swen225.lc.domain.items.Item;
+
+import java.util.ArrayList;
+
 public class Chap {
-    private final Inventory inventory;
+    private static final int MAX_INVENTORY = 8;
+    private final ArrayList<Item> inventory;
+    private int x;
+    private int y;
+
     public Chap() {
-        inventory = new Inventory();
+        inventory = new ArrayList<>();
     }
 
-    public boolean move(String direction) throws IllegalMove {
-        throw new IllegalMove("Not working yet");
+    ;
+
+    public void move(Direction direction) throws IllegalMove {
+        switch (direction) {
+            case UP -> throw new IllegalMove("Temp");
+            case DOWN -> throw new IllegalMove("Temp");
+            case LEFT -> throw new IllegalMove("Temp");
+            case RIGHT -> throw new IllegalMove("Temp");
+            default -> throw new IllegalMove("Invalid move direction");
+        }
     }
+
+    public boolean addItem(Item item) throws InventoryFull {
+        if (inventory.size() >= MAX_INVENTORY) {
+            throw new InventoryFull();
+        }
+        return inventory.add(item);
+    }
+
+    public boolean removeItem(Item item) {
+        return inventory.remove(item);
+    }
+
+    public enum Direction {UP, DOWN, LEFT, RIGHT}
 }
---
+++ LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/domain/InventoryFull.java
@@ -0,0 +1,11 @@
+package nz.ac.wgtn.swen225.lc.domain;
+
+public class InventoryFull extends Exception {
+    public InventoryFull() {
+        super();
+    }
+
+    public InventoryFull(String message) {
+        super(message);
+    }
+}
