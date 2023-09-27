25a569fcf54665f9ec7226cd9177d2e580a9b855
Tokeley <137751232+Tokeley@users.noreply.github.com>
2023-09-27 16:47:11 +1300

    Renderer class now has JavaDoc comments! - Alex Manning

--- LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
+++ LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
@@ -11,20 +11,31 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * The Renderer class is responsible for rendering the game board and characters on the JPanel.
+ */
 public class Renderer {
 
     Board board;
     Tile[][] grid;
     public Camera camera = new Camera(3, 3, 9, 9);
+
+    /**
+     * Constructor for the Renderer class.
+     *
+     * @param board The game board to render.
+     */
     public Renderer(Board board){
         this.board = board;
         grid = board.getTiles();
     }
 
     /**
-     * Draws each tile to form maze
-     * @param mazePanel
-     * @param g
+     * Draws the game board, characters and items on the provided JPanel.
+     *
+     * @param mazePanel The JPanel on which to render the game.
+     * @param g         The Graphics object to use for rendering.
+     * @throws IOException If there is an error loading image files.
      */
     public void draw(JPanel mazePanel, Graphics g) throws IOException {
         int mazePanelWidth = mazePanel.getWidth();
@@ -36,16 +47,18 @@ public class Renderer {
         g.setColor(new Color(232, 220, 202));
         g.fillRect(0, 0, mazePanelWidth, mazePanelHeight);
 
+        // Dimensions for board and tile (for window resizing)
         int clampedValue = Math.max(0, Math.min(tileWidth, tileHeight));
         int distanceFromLeftBorder = mazePanelWidth/2 - (clampedValue* camera.getWidth()/2);
         int distanceFromTopBorder = mazePanelHeight/2 - (clampedValue* camera.getHeight()/2);
 
+        // Go through all tiles visible to camera and draw them on the JPanel
         for (int x = camera.getX(); x < camera.getX() + camera.getWidth(); x++){
             for (int y = camera.getY(); y < camera.getY() + camera.getHeight(); y++){
                 int cameraX = camera.worldXToCameraX(x);
                 int cameraY = camera.worldYToCameraY(y);
                 Tile tile;
-                // If coord trying to be drawn is out of bounds of board then just draw a wall tile.
+                // If coordinates trying to be drawn is out of bounds of board then just draw a wall tile.
                 if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length){
                     tile = new Wall();
                 }
@@ -57,20 +70,19 @@ public class Renderer {
             }
         }
 
-        // draw Chap
+        // Draw Chap at Chap's coordinates
         File chapFile;
         chapFile = new File("LarryCroftsAdventures" + File.separator + "assets" + File.separator + "Chap.png");
-
         Image chapImage = ImageIO.read(chapFile);
         int chapX = camera.worldXToCameraX(board.getChap().getX());
         int chapY = camera.worldYToCameraY(board.getChap().getY());
-
         g.drawImage(chapImage, chapX*clampedValue + distanceFromLeftBorder, chapY*clampedValue + distanceFromTopBorder, clampedValue, clampedValue, null);
     }
 
     private Image getTileImage(Tile tile) throws IOException {
         String fileName = "LarryCroftsAdventures" + File.separator + "assets" + File.separator;
 
+        // Assign filename depending on tile type and tile item
         switch (tile.getClass().getSimpleName()) {
             case "Door" -> {
                 Key.Colour doorColour = ((Door) tile).getColour();
@@ -99,16 +111,23 @@ public class Renderer {
         return ImageIO.read(file);
     }
 
-    public void moveCameraLeft(){
-        camera.setX(camera.getX()-1);
-    }
-    public void moveCameraRight(){
-        camera.setX(camera.getX()+1);
-    }
-    public void moveCameraUp(){
-        camera.setY(camera.getY()-1);
-    }
-    public void moveCameraDown(){
-        camera.setY(camera.getY()+1);
-    }
+    /**
+     * Move the camera one tile to the left.
+     */
+    public void moveCameraLeft() { camera.setX(camera.getX() - 1); }
+
+    /**
+     * Move the camera one tile to the right.
+     */
+    public void moveCameraRight() { camera.setX(camera.getX() + 1); }
+
+    /**
+     * Move the camera one tile up.
+     */
+    public void moveCameraUp() { camera.setY(camera.getY() - 1); }
+
+    /**
+     * Move the camera one tile down.
+     */
+    public void moveCameraDown() { camera.setY(camera.getY() + 1); }
 }
