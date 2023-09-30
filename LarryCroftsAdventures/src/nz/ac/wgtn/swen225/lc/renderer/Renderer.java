74eada1a1f7b733de7c3f26b8f57aed22272b52c
Tokeley <137751232+Tokeley@users.noreply.github.com>
2023-09-30 13:03:38 +1300

    Improved image loading so game runs waaaay better now - Alex Manning

--- LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
+++ LarryCroftsAdventures/src/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
@@ -8,12 +8,12 @@ import nz.ac.wgtn.swen225.lc.domain.tiles.*;
 import javax.swing.*;
 import java.awt.*;
 import javax.imageio.ImageIO;
+import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * The Renderer class is responsible for rendering the game board and characters on the JPanel.
@@ -27,16 +27,20 @@ public class Renderer {
     public enum State {
         IDLE, UP, DOWN, LEFT, RIGHT
     }
+    private enum Images {
+        CHAP, DOOR_BLUE, DOOR_GREEN, DOOR_RED, DOOR_YELLOW, EXIT, FREE, INFOBOX, KEY_BLUE, KEY_GREEN, KEY_RED, KEY_YELLOW, TREASURE, WALL
+    }
     private State state = State.IDLE;
     private Timer timer;
     private double distanceTotal = 0;
     private double distance = 0.125;
+    private HashMap<Images, Image> images = new HashMap<>();
     /**
      * Constructor for the Renderer class.
      *
      * @param board The game board to render.
      */
-    public Renderer(Board board, JPanel panel){
+    public Renderer(Board board, JPanel panel) throws IOException {
         panel = new JPanel(){
             @Override
             public void paint(Graphics g) {
@@ -51,10 +55,11 @@ public class Renderer {
         grid = board.getTiles();
         this.boardPanel = panel;
         this.camera = new Camera(board.getChap().getTile().getX() - 4, board.getChap().getTile().getY() - 4, 9, 9);
+        loadImages();
         startTimer();
     }
 
-    public void startTimer(){
+    private void startTimer(){
         timer = new Timer(10, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {;
@@ -63,6 +68,25 @@ public class Renderer {
         });
         timer.start();
     }
+
+    private void loadImages() throws IOException {
+        String path = "LarryCroftsAdventures/assets/";
+        images.put(Images.CHAP, ImageIO.read(new File(path + "Chap.png")));
+        images.put(Images.DOOR_BLUE, ImageIO.read(new File(path + "Door_BLue.png")));
+        images.put(Images.DOOR_GREEN, ImageIO.read(new File(path + "Door_Green.png")));
+        images.put(Images.DOOR_RED, ImageIO.read(new File(path + "Door_Red.png")));
+        images.put(Images.DOOR_YELLOW, ImageIO.read(new File(path + "Door_Yellow.png")));
+        images.put(Images.EXIT, ImageIO.read(new File(path + "Exit.png")));
+        images.put(Images.FREE, ImageIO.read(new File(path + "Free.png")));
+        images.put(Images.INFOBOX, ImageIO.read(new File(path + "InfoBox.png")));
+        images.put(Images.KEY_BLUE, ImageIO.read(new File(path + "Key_Blue.png")));
+        images.put(Images.KEY_GREEN, ImageIO.read(new File(path + "Key_Green.png")));
+        images.put(Images.KEY_RED, ImageIO.read(new File(path + "Key_Red.png")));
+        images.put(Images.KEY_YELLOW, ImageIO.read(new File(path + "Key_Yellow.png")));
+        images.put(Images.TREASURE, ImageIO.read(new File(path + "Treasure.png")));
+        images.put(Images.WALL, ImageIO.read(new File(path + "Wall.png")));
+    }
+
 
     /**
      * Draws the game board, characters and items on the provided JPanel.
@@ -140,35 +164,37 @@ public class Renderer {
     }
 
     private Image getTileImage(Tile tile) throws IOException {
-        String fileName = "LarryCroftsAdventures" + File.separator + "assets" + File.separator;
+        Image img = null;
         // Assign filename depending on tile type and tile item
         switch (tile.getClass().getSimpleName()) {
             case "Door" -> {
                 Key.Colour doorColour = ((Door) tile).getColour();
-                fileName += "Door_" + doorColour.name();
+                if (doorColour == Key.Colour.RED){ img = images.get(Images.DOOR_RED);}
+                if (doorColour == Key.Colour.BLUE){ img = images.get(Images.DOOR_BLUE);}
+                if (doorColour == Key.Colour.GREEN){ img = images.get(Images.DOOR_GREEN);}
+                if (doorColour == Key.Colour.YELLOW){ img = images.get(Images.DOOR_YELLOW);}
             }
-            case "Exit", "ExitLock" -> fileName += "Exit";
-            case "Treasure" -> fileName += "Treasure";
-            case "InfoField" -> fileName += "InfoBox";
+            case "Exit", "ExitLock" -> img = images.get(Images.EXIT);
+            case "Treasure" -> img = images.get(Images.TREASURE);
+            case "InfoField" -> img = images.get(Images.INFOBOX);
             case "KeyTile" -> {
                 Key.Colour keyColour = ((KeyTile) tile).getColour();
-                fileName += "Key_" + keyColour.name();
+                if (keyColour == Key.Colour.RED){ img = images.get(Images.KEY_RED);}
+                if (keyColour == Key.Colour.BLUE){ img = images.get(Images.KEY_BLUE);}
+                if (keyColour == Key.Colour.GREEN){ img = images.get(Images.KEY_GREEN);}
+                if (keyColour == Key.Colour.YELLOW){ img = images.get(Images.KEY_YELLOW);}
             }
             case "Free" -> {
                 //if (((Free) tile).getItem() != null) fileName += ((Free) tile).getItem();
                 //else
-                fileName += "Free";
+                img = images.get(Images.FREE);
             }
-            case "Wall" -> fileName += "Wall";
+            case "Wall" -> img = images.get(Images.WALL);
             default -> {
-                return null; // Unknown tile type
+                return img; // Unknown tile type
             }
         }
-
-        fileName += ".png";
-        File file = new File(fileName);
-
-        return ImageIO.read(file);
+        return img;
     }
 
     public State getState(){ return state; }
