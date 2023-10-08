2cab5480d6c3d30833dc16347d2e03a6d525984c
SlazengerV100 <kendreanth@ecs.vuw.ac.nz>
2023-10-09 10:54:43 +1300

    Added JUnit tests for chap movement

--- LarryCroftsAdventures/src/test/nz/ac/wgtn/swen225/lc/domain/DomainTests.java
+++ LarryCroftsAdventures/src/test/nz/ac/wgtn/swen225/lc/domain/DomainTests.java
@@ -1,7 +1,10 @@
 package test.nz.ac.wgtn.swen225.lc.domain;
 
 import nz.ac.wgtn.swen225.lc.domain.*;
+import nz.ac.wgtn.swen225.lc.renderer.AudioUnit;
 import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DomainTests {
     private static Board board;
@@ -10,9 +13,74 @@ public class DomainTests {
     /**
      * Initialise the board and player.
      */
-    @BeforeAll
-    public static void init() {
-        board = new Board(1, null);
+    @BeforeEach
+    public void init() {
+        AudioUnit au = new AudioUnit();
+        board = new Board(1, au);
         chap = board.getChap();
+    }
+
+    @Test
+    public void testBoard() {
+
+    }
+
+    @Test
+    public void testMoveLeft() {
+        int x = chap.getX();
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.LEFT));
+        assertEquals(x - 1, chap.getX());
+    }
+
+    @Test
+    public void testMoveLeftIllegal() {
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.UP));
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.LEFT));
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.LEFT));
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.LEFT));
+        assertThrows(IllegalMove.class, () -> chap.move(Chap.Direction.LEFT));
+    }
+
+    @Test
+    public void testMoveRight() {
+        int x = chap.getX();
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.RIGHT));
+        assertEquals(x + 1, chap.getX());
+    }
+
+    @Test
+    public void testMoveRightIllegal() {
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.UP));
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.RIGHT));
+        assertThrows(IllegalMove.class, () -> chap.move(Chap.Direction.RIGHT));
+    }
+
+    @Test
+    public void testMoveDown() {
+        int y = chap.getY();
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.UP));
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.DOWN));
+        assertEquals(y, chap.getY());
+    }
+
+    @Test
+    public void testMoveDownIllegal() {
+        assertThrows(IllegalMove.class, () -> chap.move(Chap.Direction.DOWN));
+    }
+
+    @Test
+    public void testMoveUp() {
+        int y = chap.getY();
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.UP));
+        assertEquals(y - 1, chap.getY());
+    }
+
+    @Test
+    public void testMoveUpIllegal() {
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.UP));
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.UP));
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.UP));
+        assertDoesNotThrow(() -> chap.move(Chap.Direction.UP));
+        assertThrows(IllegalMove.class, () -> chap.move(Chap.Direction.UP));
     }
 }
